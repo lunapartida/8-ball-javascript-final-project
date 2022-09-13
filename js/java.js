af3bfa44f44e6795dc85cfdea5bdf572c9b7f549
Luna Partida <lunapartida93@icloud.com>
2022-09-12 17:18:58 -0700

    8 Ball Initiation

---
+++ js/java.js
@@ -0,0 +1,13 @@
+import randomcolor from "randomcolor"
+
+const advice = [
+    "If you’re good at something, never do it for free.",
+    "Do, or do not. There is no try.",
+    "It’s what you do right now that makes a difference.",
+    "Until you start believing in yourself, you ain't gonna have a life!",
+    "Life does not stop and start at your convenience.",
+    "You know what the trouble about real life is? There's no danger music."
+]
+
+// generate random light colors: 
+    // adviceEye.style.backgroundColor = randomcolor({luminosity: 'light'})
