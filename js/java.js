4ee1dd88c6dd75bad6b9410c9fa8cae7660576d4
Luna Partida <lunapartida93@icloud.com>
2022-09-13 22:33:04 -0700

    Magic 8 Ball almost finished

--- js/java.js
+++ js/java.js
@@ -1,23 +1,49 @@
-// import randomcolor from "randomcolor"
+const points = [
+  "It is certain",
+  "Without a doubt",
+  "You may rely on it",
+  "Yes definitely",
+  "It is decidedly so",
+  "As I see it, yes",
+  "Most likely",
+  "Yes",
+  "Outlook good",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Better not tell you now",
+  "Ask again later",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don’t count on it",
+  "Outlook not so good",
+  "My sources say no",
+  "Very doubtful",
+  "My reply is no",
+];
 
-let adviceDisplay = document.getElementById ('adviceDisplay')
-let button = document.getElementById('adviceBtn')
+document.getElementById("response").innerHTML = points;
 
-const advice = [
-    "If you’re good at something, never do it for free.",
-    "Do, or do not. There is no try.",
-    "It’s what you do right now that makes a difference.",
-    "Until you start believing in yourself, you ain't gonna have a life!",
-    "Life does not stop and start at your convenience.",
-    "You know what the trouble about real life is? There's no danger music."
-]
+function myFunction() {
+  points.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
+  document.getElementById("response").innerHTML = points[0];
+  document.getElementById("response").style.fontSize = "18px";
+  setTimeout(timeup, 4000);
+}
 
-button.addEventListener ('click', () => {
-    let randomAdvice = Math.floor (Math.random() * advice.length);
-    adviceDisplay.textContent = advice[randomAdvice]
-    adviceEye.style.backgroundColor = randomcolor ({luminosity: 'light'})
-});
+// $(document).ready(function () {
+//     points.getAnswer = function (question) {
+//       $(".ball").effect("shake");
+//       $("#response").text(answer);
+//       $("#response").fadeIn(3000);
+//     };
+//     const onClick = Response();
+//     {
+//       $("#respone").hide();
+//       $(".ball").attr;
+//       magic8Ball.getAnswer(response);
+//     }
 
-
-// generate random light colors: 
-    // adviceEye.style.backgroundColor = randomcolor({luminosity: 'light'})
+//     $("#askme").click(onClick);
+//   });
